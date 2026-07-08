3ce2ac6a9390fbcbf0041ccd314dd2a599c78053
Aydın Yılmaz <aydinyilmaz37883788@gmail.com>
2026-07-08 20:08:16 +0000

    Dogru kimlik (com.aydinyilmaz) ve magaza muhru eklendi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         }
     }
 
-    namespace = "com.example.medconnect"
+    namespace = "com.aydinyilmaz.medconnect"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.medconnect"
+        applicationId = "com.aydinyilmaz.medconnect"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/aydinyilmaz/medconnect/MainActivity.kt
+++ android/app/src/main/kotlin/com/aydinyilmaz/medconnect/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.medconnect
+package com.aydinyilmaz.medconnect
 
 import io.flutter.embedding.android.FlutterActivity
 
